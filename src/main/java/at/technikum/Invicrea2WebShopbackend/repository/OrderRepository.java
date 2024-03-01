2f742fbee57d2107c73eb23bd726f0e7efe80ba6
ww23e02 <ww23e022@technikum.wien.at>
2024-03-01 11:27:22 +0100

    initial commit

--- src/main/java/at/technikum/Invicrea2WebShopbackend/repository/OrderRepository.java
+++ src/main/java/at/technikum/Invicrea2WebShopbackend/repository/OrderRepository.java
@@ -1,12 +1,12 @@
 package at.technikum.Invicrea2WebShopbackend.repository;
 
 import at.technikum.Invicrea2WebShopbackend.model.Order;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Repository interface for accessing database operations related to order history objects.
  */
 @Repository
-public interface OrderRepository extends ListCrudRepository<Order, String> {
+public interface OrderRepository extends JpaRepository<Order, String> {
 }
